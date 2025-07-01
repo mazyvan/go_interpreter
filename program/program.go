87d9615f657e0a728bf180410b02c4fdc3987177
Iván Sánchez <mazyvan@hotmail.com>
2025-07-01 15:57:54 -0600

    CreateBaseProgram helper

--- program/program.go
+++ program/program.go
@@ -32,3 +32,20 @@ func CreateProgramFromFile(filePath string, eval func(ast.Node, *object.Environm
 	eval(program, env)
 	return program, env
 }
+
+func CreateBaseProgram(eval func(ast.Node, *object.Environment) object.Object) (*ast.Program, *object.Environment) {
+	utilsContent := utils.GetUtilsContent()
+	l := lexer.New(utilsContent)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		fmt.Println("Parser errors:")
+		for _, msg := range p.Errors() {
+			fmt.Printf("\t%s\n", msg)
+		}
+		os.Exit(1)
+	}
+	env := object.NewEnvironment()
+	eval(program, env)
+	return program, env
+}
