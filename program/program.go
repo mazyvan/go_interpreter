464d1616aa99fb44478ce3e4582e7c58b4b3a22f
Iván Sánchez <mazyvan@hotmail.com>
2025-07-01 13:30:48 -0600

    Program builder

---
+++ program/program.go
@@ -0,0 +1,34 @@
+package program
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"persistio/ast"
+	"persistio/lexer"
+	"persistio/object"
+	"persistio/parser"
+	"persistio/utils"
+)
+
+func CreateProgramFromFile(filePath string, eval func(ast.Node, *object.Environment) object.Object) (*ast.Program, *object.Environment) {
+	basePath := filepath.Dir(filePath)
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	utilsContent := utils.GetUtilsContent()
+	l := lexer.New(utilsContent + string(content))
+	p := parser.New(l)
+	program := p.ParseProgramFromFile(basePath)
+	if len(p.Errors()) != 0 {
+		fmt.Println("Parser errors:")
+		for _, msg := range p.Errors() {
+			fmt.Printf("\t%s\n", msg)
+		}
+		os.Exit(1)
+	}
+	env := object.NewEnvironment()
+	eval(program, env)
+	return program, env
+}
